77b8b299eec05f4892cb693008a35676fc3c4547
David Anaya <danaya@gmail.com>
2018-02-04 18:59:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <transition name="slide-fade">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
---
+++ src/services/reddit.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+
+export default {
+  getPosts(value) {
+    return Vue.http.get(`https://www.reddit.com/r/${value}/top/.json?limit=20&sort=hot`);
+  }
+};
